dc0696816165dc762d6a2acd42dcd898f852bd7a
WAGNER RIBEIRO <46025838+ramoswrr@users.noreply.github.com>
2024-02-19 11:44:23 -0300

    Aula 130 - Admin - Produtos Paginação e Busca

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -176,56 +176,56 @@ class Product extends Model {
 
 	}
 
-	// public static function getPage($page = 1, $itemsPerPage = 10)
-	// {
+	public static function getPage($page = 1, $itemsPerPage = 10)
+	{
 
-	// 	$start = ($page - 1) * $itemsPerPage;
+		$start = ($page - 1) * $itemsPerPage;
 
-	// 	$sql = new Sql();
+		$sql = new Sql();
 
-	// 	$results = $sql->select("
-	// 		SELECT SQL_CALC_FOUND_ROWS *
-	// 		FROM tb_products 
-	// 		ORDER BY desproduct
-	// 		LIMIT $start, $itemsPerPage;
-	// 	");
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_products 
+			ORDER BY desproduct
+			LIMIT $start, $itemsPerPage;
+		");
 
-	// 	$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
 
-	// 	return [
-	// 		'data'=>$results,
-	// 		'total'=>(int)$resultTotal[0]["nrtotal"],
-	// 		'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
-	// 	];
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
 
-	// }
+	}
 
-	// public static function getPageSearch($search, $page = 1, $itemsPerPage = 10)
-	// {
+	public static function getPageSearch($search, $page = 1, $itemsPerPage = 10)
+	{
 
-	// 	$start = ($page - 1) * $itemsPerPage;
+		$start = ($page - 1) * $itemsPerPage;
 
-	// 	$sql = new Sql();
+		$sql = new Sql();
 
-	// 	$results = $sql->select("
-	// 		SELECT SQL_CALC_FOUND_ROWS *
-	// 		FROM tb_products 
-	// 		WHERE desproduct LIKE :search
-	// 		ORDER BY desproduct
-	// 		LIMIT $start, $itemsPerPage;
-	// 	", [
-	// 		':search'=>'%'.$search.'%'
-	// 	]);
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_products 
+			WHERE desproduct LIKE :search
+			ORDER BY desproduct
+			LIMIT $start, $itemsPerPage;
+		", [
+			':search'=>'%'.$search.'%'
+		]);
 
-	// 	$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
 
-	// 	return [
-	// 		'data'=>$results,
-	// 		'total'=>(int)$resultTotal[0]["nrtotal"],
-	// 		'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
-	// 	];
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
 
-	// }
+	}
 
 }
 
